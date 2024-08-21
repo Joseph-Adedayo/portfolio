7c6ee132b07e3b207c97e213093b06f2c794e541
Joseph Adedayo <joseph.adedayo2@mail.dcu.ie>
2024-08-21 22:36:42 +0100

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import "@/styles/globals.css";
-import Head from 'next/head';
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-
   return (
     <>
       <Head>
         <title>Joseph Adedayo</title>
       </Head>
+      <Analytics />
       <Component {...pageProps} />
     </>
   );
-
 }
